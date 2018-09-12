668875aa6637942f331d4f8db6e450d40100b8b3
Billtrbl <sbillysusanto@yccc.edu>
2018-09-12 19:02:16 -0400

    second commit

---
+++ CISYCCC-FALL2018/src/User.java
@@ -0,0 +1,38 @@
+
+public class User 
+{
+	private String userName;
+	private String email;
+	private String firstName;
+	private String lastName;
+	
+	// Method signature consists of...
+	// Accessor - public, private or protected.
+	// Return type - ANy type or void for nothing.
+	// Method name - make it good so people know what the method does.
+	// Argument list - parameters passed to the method.
+	public String getUsername() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getfirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getlastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
