7d9138fd40937a77444dbae3553e7f93b8da8965
Billtrbl <sbillysusanto@yccc.edu>
2018-09-12 18:26:42 -0400

    second commit

--- CISYCCC-FALL2018/src/HELLOWORLD.java
+++ CISYCCC-FALL2018/src/HELLOWORLD.java
@@ -1,11 +1,24 @@
 
-public class HELLOWORLD {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Hello YCCC-CIS Fall Class");
-		System.out.println(2+2);
+public class HELLOWORLD 
+{
+	// An object is 2 things.
+	// 1. Actions. Methods
+	// 2. Attributes. Variables.
+	private String name = "Billy";
+	private String lastName = "Susanto";
+	
+	// main method. Starts the program.
+	// Method Signature - What makes this method unique. Defines the method.
+	public static void main(String[] args) 
+	{
+		HELLOWORLD hw = new HELLOWORLD();
+		hw.sayHello();
+	}	
+		
+	// This method says hello when called.
+	private void sayHello()
+	{
+		// Prints to the console.
+				System.out.println("Hello World");	
 	}
-
 }
